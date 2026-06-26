8ab0d6bffabf2351528b80a06246a9020a14c89f
Pradip <chavanpradip1995@gmail.com>
2026-06-26 05:41:07 +0530

    SB-Redis-App added

---
+++ SB-Redis-App/src/main/java/com/pradip/SbRedisAppApplication.java
@@ -0,0 +1,13 @@
+package com.pradip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRedisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRedisAppApplication.class, args);
+	}
+
+}
---
+++ SB-Redis-App/src/test/java/com/pradip/SbRedisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRedisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
